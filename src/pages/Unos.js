76172aa8d9d80e555f75453ac450d3553638f360
Shuky25 <vojinsundovic@gmail.com>
2022-06-14 00:03:31 +0200

    obradjeno

--- src/pages/Unos.js
+++ src/pages/Unos.js
@@ -2,37 +2,38 @@ import "./Unos.css";
 import { useNavigate } from "react-router-dom";
 
 const Unos = () => {
-  let txt="";
+    let txt = "";
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+  
+    const prosledi = (e) => {
+      e.preventDefault();
+      navigate("../Aplikacija", {state: {ime: txt}});
+    };
+  
+    const kupljenje = (e) => {
+      if (e.target.value != null)
+        txt = e.target.value;
+      else
+        txt = "ovde se unosi tekst";
+    };
 
-  const prosledi = (e) => {
-    e.preventDefault();
-    navigate("../Aplikacija", {state: {ime: txt}});
-  };
 
-  const kupljenje = (e) => {
-    if (e.target.value != null)
-      txt = e.target.value;
-    else
-      txt = "ovde se unosi tekst";
-  };
-
-  return (
-    <>
-      <h1>Unos</h1>
-      <form onSubmit={prosledi}>
-        <div>
-          <label>Tekst: </label>
-          <br />
-          <input type="text" placeholder="Unesite text" onChange={kupljenje} />
-        </div>
-        <button type="submit" className="dugme">
-          Unesi
-        </button>
-      </form>
-    </>
-  );
+    return (
+        <>
+            <h1>Unos</h1>
+            <form onSubmit={prosledi}>
+                <div>
+                    <label>Tekst: </label>
+                    <br />
+                    <input name="tekst" id="tekst" type="text" placeholder="Unesite text" onChange={kupljenje}/>
+                </div>
+                <button type="submit" className="dugme">
+                    Unesi
+                </button>
+            </form>
+        </>
+    );
 };
 
 export default Unos;
