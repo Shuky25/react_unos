daebe537a48374f3b2457e3a70338114326341b0
Shuky25 <vojinsundovic@gmail.com>
2022-06-14 00:04:39 +0200

    v

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Aplikacija from "./pages/Aplikacija";
-import Layout from "./pages/Layout";
-import Unos from "./pages/Unos";
 import './Footer.css';
 
 function App() {
